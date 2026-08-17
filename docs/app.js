cbf089bd0b2d5ab1bbc4c8c6542184b2cc15d4ca
R Virinchi <rvirinchi02@gmail.com>
2026-08-17 21:06:40 +0530

    Fixed scaling issues on mobile

--- docs/app.js
+++ docs/app.js
@@ -84,14 +84,16 @@ function draw(e) {
     e.preventDefault();
 
     const rect = canvas.getBoundingClientRect();
+    const scaleX = canvas.width / rect.width;
+    const scaleY = canvas.height / rect.height;
     let x, y;
 
     if (e.type.includes('touch')) {
-        x = e.touches[0].clientX - rect.left;
-        y = e.touches[0].clientY - rect.top;
+        x = (e.touches[0].clientX - rect.left) * scaleX;
+        y = (e.touches[0].clientY - rect.top) * scaleY;
     } else {
-        x = e.clientX - rect.left;
-        y = e.clientY - rect.top;
+        x = (e.clientX - rect.left) * scaleX;
+        y = (e.clientY - rect.top) * scaleY;
     }
 
     ctx.lineTo(x, y);
